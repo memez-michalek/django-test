2bd25a8781b1a09ba10c62c779e956fbbcd3d58f
memez-michalek <michal.lesniowski1@protonmail.com>
2021-10-21 23:17:59 +0200

    third task

--- analytics/admin.py
+++ analytics/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from analytics.models import Event
 
 # Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    search_fields = ['name']
 
-admin.site.register(Event)
+
+admin.site.register(Event, EventAdmin)
