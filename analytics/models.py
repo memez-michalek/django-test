e9e34a9fde58d9567ab42f0cfc9d495033ead9c2
memez-michalek <michal.lesniowski1@protonmail.com>
2021-10-23 23:01:04 +0200

    sixth task

--- analytics/models.py
+++ analytics/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+class User(AbstractUser):
+    api_token = models.CharField(max_length=100, blank=True, unique=True)
+
 class Event(models.Model):
     name = models.CharField(max_length=255, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     additional_data = models.TextField(blank=True)
-
-class User(AbstractUser):
-    api_token = models.CharField(max_length=100, blank=True, unique=True)
-
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
