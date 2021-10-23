a092ddd10130c5e397b718eb8cc5b95a9c52bee9
memez-michalek <michal.lesniowski1@protonmail.com>
2021-10-23 22:51:52 +0200

    task 5

--- analytics/admin.py
+++ analytics/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from analytics.models import Event
+from analytics.models import Event, User
 
-# Register your models here.
 class EventAdmin(admin.ModelAdmin):
     search_fields = ['name']
 
-
 admin.site.register(Event, EventAdmin)
+admin.site.register(User)
--- analytics/models.py
+++ analytics/models.py
@@ -1,7 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 
 class Event(models.Model):
     name = models.CharField(max_length=255, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     additional_data = models.TextField(blank=True)
+
+class User(AbstractUser):
+    api_token = models.CharField(max_length=100, blank=True, unique=True)
+
